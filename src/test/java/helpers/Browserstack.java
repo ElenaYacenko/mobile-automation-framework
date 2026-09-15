ef45c934f2a95049f36d53f7632a424f2d7d52f7
ElenaYacenko <149763977+ElenaYacenko@users.noreply.github.com>
2026-09-15 16:05:27 +0300

    feat: add robust open article test avoiding isDisplayed bug

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -7,12 +7,14 @@ public class Browserstack {
 
     // curl -u "qaguru_ti9G5S:5yrxu4nFTKkRExUAhqxh" -X GET "https://api.browserstack.com/app-automate/sessions/0359d759d2aaa4f46401dac46bd281b6d9b24943.json"
     // automation_session.video_url
+    public static final String USER = "bsuser_XQJjVN";
+    public static final String KEY = "am7jziiXHzFsVGTXeDns";
 
     public static String videoUrl(String sessionId) {
         String url = String.format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
-                .auth().basic("qaguru_ti9G5S", "5yrxu4nFTKkRExUAhqxh")
+                .auth().basic(USER, KEY)
                 .get(url)
                 .then()
                 .log().status()
